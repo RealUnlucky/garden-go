d2691d3f7773f9d4fd477c05421a96bc61cb7935
Darkcat <andrewlichao@gmail.com>
2023-08-12 16:11:38 -0700

    v0.2

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import {ImageBackground, StyleSheet, Text, View, Image, Button} from 'react-native';
+import {ImageBackground, StyleSheet, Text, View, Image, Button, useWindowDimensions} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TabView, SceneMap } from 'react-native-tab-view';
 
+const renderScene = SceneMap({
 
+  
+  first: HomeScreen,
+  second: DetailsScreen,  
+  third: Profile,
+
+});
+
+function Profile(){
+  return(
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>profile Screen</Text>
+    </View>
+  )
+}
 
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <ImageBackground source={require('./imgs/HD-wallpaper-white-shelf-shelf-white.jpg')} style={{width: '100%', height: '100%', justifyContent: 'center'}}>
-        <Button title="button" onPress={() => navigation.navigate('Details')}><Image source={require('./imgs/IMG_3484.jpg')} style={{width: 40, height: 40}}/></Button>
+       <Image source={require('./imgs/IMG_3484.jpg')} style={{width: 40, height: 40}}/>
       </ImageBackground>
     </View>
   );
@@ -23,18 +38,34 @@ function DetailsScreen({ navigation }) {
     </View>
   );
 }
-const Stack = createNativeStackNavigator();
+//const Stack = createNativeStackNavigator();
 
 function App() {
+    const layout = useWindowDimensions();
+
+    const [index, setIndex] = React.useState(1);
+    const [routes] = React.useState([
+      { key: 'third', title: 'Third' },
+      { key: 'first', title: 'First' },
+      { key: 'second', title: 'Second' },
+    ]);
   return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      tabBarPosition='bottom'
+      initialLayout={{ width: layout.width }}
+    />
     
+    /*
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-    
+    */
     
   );
 }
