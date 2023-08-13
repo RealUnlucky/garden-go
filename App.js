b98fdcde8fcc659b998831f475e0d39eaf4b8f17
mintcat3 <19940947+mintcat3@users.noreply.github.com>
2023-08-13 03:39:51 -0700

    front end prettier

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import * as React from 'react';
 import { TabView, SceneMap } from 'react-native-tab-view';
 import {Card} from 'react-native-paper'
 import CollapsibleList from './CollapsibleList';
+import { useFonts } from 'expo-font';
 
 const renderScene = SceneMap({
 
@@ -21,10 +22,10 @@ const renderScene = SceneMap({
 function Profile(){
   return(
     <View>
-      <View style={{backgroundColor: '#8cff6b'}}>
-        <View style={{padding:10, width:'100%', backgroundColor: '#00833d', height: 175}}>
+      <View style={{backgroundColor: '#bee3ba'}}>
+        <View style={{padding:10, width:'100%', backgroundColor: '#5FA052', height: 175}}>
         </View>
-        <View style={{alignItems:'center', backgroundColor: '#8cff6b'}}>
+        <View style={{alignItems:'center', backgroundColor: '#bee3ba'}}>
           <Image source={require('./imgs/IMG_3484.jpg')} style={{width:140, height:140,borderRadius:100,marginTop: -70}}></Image>
           <Text style={{fontSize: 25,fontWeight:'bold',padding:10}}>John Doe</Text>
           <Text style={{fontSize: 15,fontWeight:'bold',color: 'grey'}}>Member since August 12, 2023</Text>
@@ -38,37 +39,41 @@ function Profile(){
           <Text>Missions</Text>
         </TouchableOpacity>
         <View style={{alignSelf: 'center', flexDirection:'row',justifyContent:'center',backgroundColor:'#fff',width:'90%',padding:20,paddingBottom:22,borderRadius:10,shadowOpacity:80,elevation:15,marginTop:20}}>
-          <Image source={require('./imgs/plant.png')} style={{width:20,height:25}}></Image>
+          <Image source={require('./imgs/pottedplant.png')} style={{width:20,height:25}}></Image>
           <Text style={{marginTop:4}}>No. of Plants: 0</Text>
         </View>
         <TouchableOpacity style={{alignSelf: 'center', flexDirection:'row',justifyContent:'center',backgroundColor:'#000',width:'90%',padding:20,paddingBottom:22,borderRadius:10,shadowOpacity:80,elevation:15,marginTop:20, marginBottom:40}}>
           <Text style={{fontSize:15,color:'#fff',fontWeight:'bold',marginLeft:10}}>Logout</Text>
         </TouchableOpacity>
       </View>
-      <View style={{backgroundColor: '#8cff6b', height: 200}}></View>
+      <View style={{backgroundColor: '#bee3ba', height: 200}}></View>
     </View>
   )
 }
 
 function HomeScreen({ navigation }) {
   return (
-    <ScrollView style={{flex: 1, backgroundColor: '#13ec5a'}}>
+    <ScrollView style={{flex: 1, backgroundColor: '#5FA052'}}>
       <ImageBackground source={require('./imgs/background.jpg')} resizeMode="cover" style={{height:'150%', width:'100%'}}>
-      <Text style={{fontSize: 40, marginTop: '9%', textAlign: 'center', color: '#074415', fontWeight: 'bold'}}>
-        GardenGO
-      </Text>
-      <TouchableOpacity style={{marginTop: '5%', backgroundColor: '#c7f9d2', width: '50%', marginLeft: '25%', height: '50%', justifyContent: 'center'}}
-        onPress={console.log('hi')}>
-          <Image source={require('./imgs/plant.png')} style={{width: '100%', height: '100%'}}></Image>
-      </TouchableOpacity>
-      <View style={{marginTop: '5%', alignContent: 'center'}}>
-        <Text style={{textAlign: 'center', fontSize: 20, color: 'green'}}>
-          (Plant) Information
-        </Text>
-        <Text style={{marginLeft: '2%', marginTop: '2%', fontSize: 15, color: 'green'}}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-        </Text>
-      </View>
+        
+          <Text style={{marginTop: '18%', fontSize: 40, textAlign: 'center', color: '#074415', fontWeight: 'bold'}}>
+            GardenGO
+          </Text>
+          
+          <TouchableOpacity style={{marginTop: '7%', width: '50%', marginLeft: '25%', height: '50%', justifyContent: 'center'}}
+            onPress={console.log('hi')}>
+              <Image source={require('./imgs/plant.png')} style={{width: '100%', height: '100%'}}></Image>
+          </TouchableOpacity>
+
+          <View style={{marginTop: '5%', alignContent: 'center'}}>
+            <Text style={{textAlign: 'center', fontSize: 20, color: 'green'}}>
+              (Plant) Information
+            </Text>
+            <Text style={{marginLeft: '8%', marginRight: '8%', marginTop: '2%', fontSize: 15, color: 'green'}}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+            </Text>
+          </View>
+
       </ImageBackground>
     </ScrollView>
   );
@@ -80,7 +85,11 @@ function DetailsScreen({ navigation }) {
     <SafeAreaView style = {{flex: 1}}>
       <ScrollView>
         <View>
-          <Text style={{textAlign: 'center', fontSize: 35, marginBottom: '4%'}}>Plant Collection</Text>
+          <Text style={{textAlign: 'center', fontSize: 35, marginBottom: '4%'}}>
+            Plant Collection 
+            <Image source={require('./imgs/planticon.png')} style={{width: 50, height: 50}}></Image>
+          </Text>
+          
         </View>
 
         <View style = {{flexDirection:'row', marginTop: '5%'}}>
@@ -137,31 +146,40 @@ function AboutScreen({ navigation }) {
   ];
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <ImageBackground source={require('./imgs/green-cartoon-plant-bg.jpg')} style={{width: '100%', height: '100%', justifyContent: 'center'}}>
+    <View style={styles.container}>
+      <ImageBackground source={require('./imgs/green-cartoon-plant-bg.jpg')} style={{width: '100%', height: '100%', justifyContent: 'center'}}>
+        <SafeAreaView style={{ flex: 1 }}>
           <Text style ={styles.aboutTitle}>About</Text>
-          <SafeAreaView style={{ flex: 1 }}>
-            <ScrollView>
-              <CollapsibleList data={data} />
-            </ScrollView>
-          </SafeAreaView>
-        </ImageBackground>
-      </View>
-    </SafeAreaView>
+          <ScrollView>
+            <CollapsibleList data={data} />
+          </ScrollView>
+        </SafeAreaView>
+      </ImageBackground>
+    </View>
   );
 }
 
 function App() {
+
+    const [fontsLoaded, fontError] = useFonts({
+      'Rubik': require('./assets/fonts/Rubik/static/Rubik-Regular.ttf'),
+    });
+  
+
     const layout = useWindowDimensions();
 
     const [index, setIndex] = React.useState(1);
     const [routes] = React.useState([
-      { key: 'third', title: 'Third' },
+      { key: 'third', title: 'Profile' },
       { key: 'first', title: 'First' },
       { key: 'second', title: 'Second' },
-      { key: 'fourth', title: 'Fourth' },
+      { key: 'fourth', title: 'About' },
     ]);
+
+    if (!fontsLoaded && !fontError) {
+      return null;
+    }
+
   return (
     <TabView
       navigationState={{ index, routes }}
@@ -189,6 +207,7 @@ const styles = StyleSheet.create({
   aboutTitle: {
     fontSize: 40,
     textAlign: 'center',
+    marginBottom: 30,
   }
 });
 
