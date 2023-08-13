621e3c2eb7ae70a51c13278d8fbffa78008183ab
Darkcat <andrewlichao@gmail.com>
2023-08-12 19:39:17 -0700

    v0.3 front to back

---
+++ flask-server/server.py
@@ -0,0 +1,22 @@
+# Import flask and datetime module for showing date and time
+from flask import Flask
+import datetime
+
+x = datetime.datetime.now()
+
+# Initializing flask app
+app = Flask(__name__)
+
+
+# Route for seeing a data
+@app.route('/data')
+
+def get_time():
+
+	# Returning an api for showing in reactjs
+	return {"Date":x}
+
+	
+# Running app
+if __name__ == '__main__':
+	app.run(host = '76.170.201.91',port=3000,debug='True')
